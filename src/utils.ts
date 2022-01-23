7e51c1ec733cfd1c963147de54fded8110c9d062
JacobLinCool <jacoblincool@gmail.com>
2022-01-24 05:52:45 +0800

    feat(util): obj_to_hash & retry add cooldown param

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,22 @@
+import crypto from "crypto";
+
 export function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export async function retry<T>(fn: () => T | Promise<T>, max = 3): Promise<T> {
+export async function retry<T>(fn: () => T | Promise<T>, max = 3, cooldown = 0): Promise<T> {
     let err = null;
-    for (let i = 0; i < max; i++) {
+    for (let i = 0; i <= max; i++) {
         try {
             return await fn();
         } catch (e) {
             err = e;
         }
+        await sleep(cooldown);
     }
     throw err;
+}
+
+export function obj_to_hash(obj: any): string {
+    return crypto.createHash("md5").update(JSON.stringify(obj)).digest("hex");
 }
