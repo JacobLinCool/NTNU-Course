69a7cb084ca88738170dd4a13e3a9ba55b45cc54
JacobLinCool <jacoblincool@gmail.com>
2022-01-24 05:53:24 +0800

    feat: add query & Query class to export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import { Query } from "./Query";
+
+/**
+ * 查詢師大課程
+ */
+const query = new Query();
+
 export * from "./types";
 export * from "./meta";
+export { query, Query };
+export default query;
