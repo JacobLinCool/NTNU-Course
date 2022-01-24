1ebaff8aad4025461d2eab960e765fc2fe5b6883
JacobLinCool <jacoblincool@gmail.com>
2022-01-24 18:53:32 +0800

    test: add `query.info` tests

--- src/_tests/query.test.ts
+++ src/_tests/query.test.ts
@@ -1,8 +1,8 @@
 import query from "../";
 
-describe("meta", () => {
-    jest.setTimeout(30000);
+jest.setTimeout(30000);
 
+describe("query.meta", () => {
     it("should throw Error", async () => {
         await expect(query.meta({})).rejects.toThrow();
     });
@@ -56,6 +56,73 @@ describe("meta", () => {
             teacher: "蔣宗哲",
         });
         expect(meta).toHaveLength(3);
+        expect(Date.now() - time).toBeGreaterThan(100);
+    });
+});
+
+describe("query.info", () => {
+    it("should throw Error", async () => {
+        const meta = await query.meta({
+            serial: 963,
+        });
+        const m = JSON.parse(JSON.stringify(meta[0]));
+        m.year = 100;
+        await expect(query.info(m)).rejects.toThrow();
+    });
+
+    it("should return info of a GU course", async () => {
+        const meta = await query.meta({
+            serial: 963,
+        });
+        const info = await query.info(meta[0]);
+
+        expect(info.serial).toBe(963);
+        expect(info.hours).toBe(2);
+        expect(info.general_core).toEqual(["邏輯運算"]);
+        expect(info.goals).toHaveLength(4);
+    });
+
+    it("should return info of a CSIE course", async () => {
+        const meta = await query.meta({
+            serial: 2954,
+        });
+        const info = await query.info(meta[0]);
+
+        expect(info.serial).toBe(2954);
+        expect(info.hours).toBe(3);
+        expect(info.general_core).toEqual([]);
+        expect(info.goals).toHaveLength(2);
+        expect(info.prerequisite).toBe("◎必須先修過【CSU0001  程式設計（一）】");
+    });
+
+    it("should work with cache", async () => {
+        const time = Date.now();
+        const meta = await query.meta({
+            serial: "963",
+        });
+        const info = await query.info(meta[0]);
+
+        expect(info.serial).toBe(963);
+        expect(info.hours).toBe(2);
+        expect(info.general_core).toEqual(["邏輯運算"]);
+        expect(info.goals).toHaveLength(4);
+        expect(Date.now() - time).toBeLessThan(100);
+    });
+
+    it("should clear cache", async () => {
+        query.clear();
+
+        const meta = await query.meta({
+            serial: "963",
+        });
+
+        const time = Date.now();
+        const info = await query.info(meta[0]);
+
+        expect(info.serial).toBe(963);
+        expect(info.hours).toBe(2);
+        expect(info.general_core).toEqual(["邏輯運算"]);
+        expect(info.goals).toHaveLength(4);
         expect(Date.now() - time).toBeGreaterThan(100);
     });
 });
